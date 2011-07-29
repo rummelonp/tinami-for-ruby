313b8bd510e5a71fd4bf15d03ab74c6c3c92cc63
Kazuya Takeshima <mail@mitukiii.jp>
2011-07-29 18:16:16 +0900

    Refactoring TINAMI specs.

--- spec/tinami_spec.rb
+++ spec/tinami_spec.rb
@@ -2,10 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TINAMI do
   describe :client do
-    it { TINAMI.client.should be_a TINAMI::Client }
+    it 'should be a TINAMI::Client' do
+      TINAMI.client.should be_a TINAMI::Client
+    end
   end
 
   describe :endpoint do
-    it { TINAMI.endpoint.should == TINAMI::Configuration::DEFAULT_ENDPOINT }
+    it 'should == TINAMI::Configuration::DEFAULT_ENDPOINT' do
+      TINAMI.endpoint.should == TINAMI::Configuration::DEFAULT_ENDPOINT
+    end
   end
 end
