3f59e6b4a08630772d25e6f3fe66bf7dca83f99c
Kazuya Takeshima <mail@mitukiii.jp>
2011-07-29 18:21:51 +0900

    TINAMI delegate to TINAMI::Client.

--- lib/tinami.rb
+++ lib/tinami.rb
@@ -9,4 +9,13 @@ module TINAMI
   def client(options = {})
     TINAMI::Client.new(options)
   end
+
+  def method_missing(method_name, *args, &block)
+    return super unless client.respond_to?(method_name)
+    client.send(method_name, *args, &block)
+  end
+
+  def respond_to?(method_name)
+    return client.respond_to?(method_name) || super
+  end
 end
