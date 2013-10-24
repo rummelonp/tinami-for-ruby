be8f93eca43a5930ce7b8e1864b184b6def7fbca
Kazuya Takeshima <mail@mitukiii.jp>
2013-10-25 01:14:18 +0900

    Add optional arg include_private to TINAMI.method_missing

--- lib/tinami.rb
+++ lib/tinami.rb
@@ -19,7 +19,7 @@ module TINAMI
   end
 
   # Delegate to {TINAMI::Client}
-  def self.respond_to?(method_name)
-    return client.respond_to?(method_name) || super
+  def self.respond_to?(method_name, include_private = false)
+    return client.respond_to?(method_name, include_private) || super
   end
 end
